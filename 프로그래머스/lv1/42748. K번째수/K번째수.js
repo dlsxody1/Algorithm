9855bd3b5fa65327ed505af557ed5c53bb21490f
김인태 <62875596+dlsxody1@users.noreply.github.com>
2023-05-03 10:37:36 +0900

    Update K번째수.js

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -3,6 +3,8 @@ function solution(array, commands) {
       var answer = [];
     commands.map((el) => {
           
+        // sort() 메소드를 사용할 때는 안에 값을 넣어주지 않으면 문자열 배열을 리턴해버린다...!!
+        // 조심!
         return answer.push(array.slice(el[0]-1, el[1]).sort((a,b) => a-b)[el[2]-1]);
     })
     return answer;
