f48e09965c1a84b92d092d9891e81ef0906e2e09
김인태 <62875596+dlsxody1@users.noreply.github.com>
2023-05-03 10:36:18 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,12 +1,18 @@
 function solution(array, commands) {
     var answer = [];
+      var answer = [];
     commands.map((el) => {
-        const i = el[0] - 1;
-        const j = el[1] - 1;
-        const k = el[2] - 1;
-        const sliced = array.slice(i, j + 1);
-        const sorted = sliced.sort((a, b) => a - b);
-        answer.push(sorted[k]);
-    });
+          
+        return answer.push(array.slice(el[0]-1, el[1]).sort((a,b) => a-b)[el[2]-1]);
+    })
     return answer;
+    // commands.map((el) => {
+    //     const i = el[0] - 1;
+    //     const j = el[1] - 1;
+    //     const k = el[2] - 1;
+    //     const sliced = array.slice(i, j + 1);
+    //     const sorted = sliced.sort((a, b) => a - b);
+    //     answer.push(sorted[k]);
+    // });
+    // return answer;
 }
