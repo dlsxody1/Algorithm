499c9315e0a1f798932818d6eae835e761ac6a6e
김인태 <62875596+dlsxody1@users.noreply.github.com>
2023-11-17 15:50:27 +0900

    [level 2] Title: 더 맵게, Time: 446.11 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,96 @@
+// function solution(scoville, K) {
+//     var answer = 0;
+//     let sum = 0;
+//     let idx = 0
+//     //섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+//     scoville.sort((a,b) => a-b);
+//     while(sum < K){
+//         sum = scoville[idx] + scoville[idx+1] * 2;
+//         if(sum < K){ 
+//             scoville.shift();
+//             scoville.shift();
+//             scoville.unshift(sum);         
+//             answer++;
+            
+//         }else{
+//             answer++;
+//             break;
+//         }
+//     }
+//     return answer;
+// }
+//효율성 테스트 실패
+
+
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+    size() { return this.heap.length; }
+    peek() { return this.heap[0]; }
+    
+    push(value) {
+        this.heap.push(value);
+        let index = this.heap.length - 1;
+
+        while (
+          index > 0 &&
+          this.heap[index] < this.heap[Math.floor((index - 1) / 2)]
+        ) {
+            const temp = this.heap[index];
+            this.heap[index] = this.heap[Math.floor((index - 1) / 2)];
+            this.heap[Math.floor((index - 1) / 2)] = temp;
+            index = Math.floor((index - 1) / 2);
+        }
+    }
+
+    // 값을 빼되, 오름차 순 정렬 함
+    pop() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+
+        const minValue = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        let index = 0;
+
+        while (index * 2 + 1 < this.heap.length) {
+            let minChildIndex = 
+              (index * 2 + 2 < this.heap.length 
+                && this.heap[index*2 + 2] < this.heap[index*2 + 1]) 
+                ? index * 2 + 2 : index * 2 + 1;
+            
+            if (this.heap[index] < this.heap[minChildIndex]) {
+                break;
+            }
+
+            const temp = this.heap[index];
+            this.heap[index] = this.heap[minChildIndex];
+            this.heap[minChildIndex] = temp;
+            index = minChildIndex;
+        }
+        return minValue;
+    }
+}
+
+function solution(scoville, K) {
+  const minHeap = new MinHeap();
+
+  for (const sco of scoville) {
+    minHeap.push(sco);
+  }
+
+  let mixedCount = 0;
+
+  // 힙 안에 음식이 2개 이상 있고
+  // 스코빌 지수가 가장 작은 음식의 수치가 K보다 작을 때까지 반복
+  while (minHeap.size() >= 2 && minHeap.peek() < K) {
+    // 스코빌 지수가 가장 작은 두 음식 섞기
+    const first = minHeap.pop();
+    const second = minHeap.pop();
+    const mixedFood = first + second * 2;
+    minHeap.push(mixedFood);
+    mixedCount++;
+  }
+
+  return minHeap.peek() >= K ? mixedCount : -1;
+}
