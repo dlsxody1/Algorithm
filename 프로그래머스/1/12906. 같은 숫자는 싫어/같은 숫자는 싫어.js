0d370658065f92bb7f3ec83832aeb977ca52ef75
김인태 <62875596+dlsxody1@users.noreply.github.com>
2024-01-26 14:10:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.53 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,7 @@
 function solution(arr){
     let answer = [];
     for(let i = 0 ; i < arr.length ; i++){
-        if(arr[i] === arr[i+1]){
-            
-        }else{
+        if(arr[i] !== arr[i+1]){
             answer.push(arr[i]);
         }
     }
