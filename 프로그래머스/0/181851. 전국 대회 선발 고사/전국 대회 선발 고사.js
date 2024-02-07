14f988b6d8a46d360ada2200e11f803a561e9cca
김인태 <62875596+dlsxody1@users.noreply.github.com>
2024-02-07 15:38:22 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 3.17 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -0,0 +1,10 @@
+function solution(rank, attendance) {
+    const RankArr = rank.map((el,i) => {
+        return { ranker : el , participation : attendance[i], index : i}
+    }).filter((el) => el.participation === true).sort((a,b) => a.ranker - b.ranker).slice(0,3);
+    console.log(RankArr)
+    // ;
+    
+    
+    return RankArr[0].index * 10000 + RankArr[1].index * 100 + RankArr[2].index;
+}
