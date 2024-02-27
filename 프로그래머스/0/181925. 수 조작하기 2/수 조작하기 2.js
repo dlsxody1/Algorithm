736dd7029c3629ad49bcacd3c154ffffc05ef582
김인태 <62875596+dlsxody1@users.noreply.github.com>
2024-02-27 11:45:29 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.07 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,12 @@
+function solution(numLog) {
+    var answer = '';
+    
+    for(let i = 0 ; i < numLog.length-1 ; i++){
+        if((numLog[i] + 1)  === numLog[i+1]) answer += 'w';
+        if((numLog[i] - 1) === numLog[i+1]) answer += 's';
+        if((numLog[i] + 10) === numLog[i+1]) answer += 'd';
+        if((numLog[i] - 10) === numLog[i+1]) answer += 'a';
+    }
+    
+    return answer;
+}
