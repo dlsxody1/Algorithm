906adb2cc487e8689e4d162cb0d13ec377e94062
김인태 <62875596+dlsxody1@users.noreply.github.com>
2024-05-08 20:07:09 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,18 @@
+function solution(nums) {
+    const phoneSet = new Set(nums);
+    const phoneketmon = nums.length;
+    
+    return Math.min(phoneSet.size, phoneketmon/2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
